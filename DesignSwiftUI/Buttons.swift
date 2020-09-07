c4e55226940a051891762eee68112cfe55c72683
Daniel Esteban <esteban@urbvan.com>
2020-09-07 18:40:17 -0500

    Add SwiftUI part 3 lesson 3 updates

--- DesignSwiftUI/Buttons.swift
+++ DesignSwiftUI/Buttons.swift
@@ -9,12 +9,34 @@
 import SwiftUI
 
 struct Buttons: View {
+    
+    var body: some View {
+        VStack(spacing: 50) {
+            RectangleButton()
+            
+            CircleButton()
+            
+        }
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
+        .edgesIgnoringSafeArea(.all)
+        .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
+    }
+}
+
+struct Buttons_Previews: PreviewProvider {
+    static var previews: some View {
+        Buttons()
+    }
+}
+
+struct RectangleButton: View {
     @State var tap = false
     @State var press = false
     
     var body: some View {
-        VStack {
-            Text("Button")
+        Text("Button")
             .font(.system(size: 20, weight: .semibold, design: .rounded))
             .frame(width: 200, height: 60)
             
@@ -30,7 +52,7 @@ struct Buttons: View {
                     RoundedRectangle(cornerRadius: 16, style: .continuous)
                         .fill(
                             LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9019607843, green: 0.9294117647, blue: 0.9882352941, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                            )
+                        )
                         .padding(2)
                         .blur(radius: 2)
                 }
@@ -40,18 +62,18 @@ struct Buttons: View {
             .gesture(
                 LongPressGesture(minimumDuration: 0.5, maximumDistance: 10)
                     .onChanged { value in
-                    self.tap = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        self.tap = false
+                        self.tap = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            self.tap = false
+                        }
                     }
-                }
-                .onEnded { value in
-                    self.press.toggle()
-                }
+                    .onEnded { value in
+                        self.press.toggle()
+                    }
             )
-                
+            
             .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                
+            
             .overlay(
                 HStack {
                     Image(systemName: "person.crop.circle")
@@ -65,19 +87,56 @@ struct Buttons: View {
                     Spacer()
                 }
             )
-                
+            
             .shadow(color: press ? Color(#colorLiteral(red: 0.7450980392, green: 0.8, blue: 0.8980392157, alpha: 1)) : Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 20, x: -20, y: -20)
             .shadow(color: press ? Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)) : Color(#colorLiteral(red: 0.7294117647, green: 0.7843137255, blue: 0.8941176471, alpha: 1)), radius: 20, x: 20, y: 20)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
-        .edgesIgnoringSafeArea(.all)
-        .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
     }
 }
 
-struct Buttons_Previews: PreviewProvider {
-    static var previews: some View {
-        Buttons()
+struct CircleButton: View {
+    @State var tap = false
+    @State var press = false
+
+    var body: some View {
+        ZStack {
+            Image(systemName: "sun.max")
+                .font(.system(size: 44, weight: .light))
+                .offset(x: press ? -90 : 0, y: press ? -90 : 0)
+                .rotation3DEffect(Angle(degrees: press ? 20 : 0), axis: (x: 10, y: 10, z: 0))
+            Image(systemName: "moon")
+                .font(.system(size: 44, weight: .light))
+                .offset(x: press ? 0 : 90, y: press ? 0 : 90)
+                .rotation3DEffect(Angle(degrees: press ? 0 : 20), axis: (x: 10, y: 10, z: 0))
+        }
+        .frame(width: 100, height: 100)
+        
+        .background(
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.9019607843, green: 0.9294117647, blue: 0.9882352941, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.9019607843, green: 0.9294117647, blue: 0.9882352941, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                Circle()
+                    .shadow(color: Color(#colorLiteral(red: 0.7256438732, green: 0.7787529826, blue: 0.8831245303, alpha: 1)), radius: press ? 10 : 3, x: press ? 5 : -5, y: press ? 5 : -5)
+                Circle()
+                    .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 3, x: press ? -3 : 3, y: press ? -3 : 3)
+            }
+        )
+        
+        .background(Color.white)
+        .clipShape(Circle())
+        .shadow(color: press ? Color(#colorLiteral(red: 0.7450980392, green: 0.8, blue: 0.8980392157, alpha: 1)) : Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 20, x: -20, y: -20)
+        .shadow(color: press ? Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)) : Color(#colorLiteral(red: 0.7294117647, green: 0.7843137255, blue: 0.8941176471, alpha: 1)), radius: 20, x: 20, y: 20)
+        scaleEffect(tap ? 1.2 : 1)
+        
+        .gesture(
+            LongPressGesture().onChanged { value in
+                self.tap = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    self.tap = false
+                }
+            }
+            .onEnded { value in
+                self.press.toggle()
+            }
+        )
+        
     }
 }
